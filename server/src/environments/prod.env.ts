e73bc56dbe9cb37b92223fa3d68055938333347a
“Samriddha <2000200402@iitdh.ac.in>
2022-06-14 00:20:51 +0530

    First Commit

--- server/src/environments/prod.env.ts
+++ server/src/environments/prod.env.ts
@@ -1,4 +1,5 @@
 export const ProdEnvironment = {
-  db_url: '',
+  db_url:
+    'mongodb+srv://codingclub:CODINGCLUB2022@cluster0.mmify.mongodb.net/?retryWrites=true&w=majority',
   jwt_secret: ''
 };
