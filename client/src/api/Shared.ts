0a69d8095c458324482fad81c0ccaf8cc18d2b83
“Samriddha <2000200402@iitdh.ac.in>
2022-06-14 00:30:46 +0530

    Added hosted URL

--- client/src/api/Shared.ts
+++ client/src/api/Shared.ts
@@ -1,3 +1,3 @@
-// export const baseUrl = 'https://codeforces-leaderboard.herokuapp.com/api';
+export const baseUrl = 'https://code-geass.herokuapp.com/api';
 
-export const baseUrl = 'http://localhost:5000/api';
+// export const baseUrl = 'http://localhost:5000/api';
