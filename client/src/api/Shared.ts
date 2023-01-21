09e0337d55bfc2baedcca99cd8348cbf0f41dc64
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2023-01-22 02:06:05 +0530

    Added codeforcesUrl

--- client/src/api/Shared.ts
+++ client/src/api/Shared.ts
@@ -1,2 +1,3 @@
 /* export const baseUrl = 'http://localhost:5000/api'; */
 export const baseUrl = 'https://codeforces-leaderboard-production.up.railway.app/api';
+export const codeforcesUrl = 'https://codeforces.com/api/user.info?handles=';
