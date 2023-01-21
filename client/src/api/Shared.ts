8af1df26ee8ac2047576ac6813f74b8892ea87b5
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2023-01-22 01:37:43 +0530

    Hosting

--- client/src/api/Shared.ts
+++ client/src/api/Shared.ts
@@ -1,2 +1,2 @@
 /* export const baseUrl = 'http://localhost:5000/api'; */
-export const baseUrl = 'https://codeforces-leaderboard.herokuapp.com/api';
+export const baseUrl = 'https://codeforces-leaderboard-production.up.railway.app/api';
